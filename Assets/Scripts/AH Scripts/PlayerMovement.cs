547180b317d588a629ff2fbd73cd507f4d7cc1b7
ashton1171 <ashtonjh03@gmail.com>
2022-09-06 19:00:03 +0930

    Finished PlayerMovement and added MoveCamera

--- Assets/Scripts/AH Scripts/PlayerMovement.cs
+++ Assets/Scripts/AH Scripts/PlayerMovement.cs
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    public float jumpHeight = 2f;
 
     Vector3 velocity;
 
@@ -22,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
     private Vector3 playerScale;
     private bool sneaking;
+
 
     // Update is called once per frame
     void Update()
@@ -42,46 +42,42 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-        }
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
 
 
 
         //Sneaking on/off
 
-        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
-        {
-            sneaking = true;
-            StartSneak();
+        if (Input.GetKeyDown(KeyCode.LeftControl) && sneaking == false)
+        {  
+                StartSneak();
+                sneaking = true;
+                Debug.Log("StartSneak");
         }
         if (Input.GetKeyDown(KeyCode.LeftControl) && sneaking == true)
         {
-            StopSneak();
+                StopSneak();
+                sneaking = false;
+                Debug.Log("StopSneak");
         }
+        
     }
+
 
     private void StartSneak()
     {
-        if (sneaking == false && isGrounded)
+        
             transform.localScale = crouchScale;
         transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-        
+
     }
 
     private void StopSneak()
     {
-        if ( sneaking == true)
-        {
+       
+        
             transform.localScale = playerScale;
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
-            sneaking = false;
-        }
+        
     }
 }
 

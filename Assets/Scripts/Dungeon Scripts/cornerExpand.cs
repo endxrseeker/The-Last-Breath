f5c7b0df7e37db454034b904ebfdbe8c7fe5846a
endxrseeker <ashtonm1245@gmail.com>
2022-09-08 21:16:50 +1000

    textures and models for dungeon

--- Assets/Scripts/Dungeon Scripts/cornerExpand.cs
+++ Assets/Scripts/Dungeon Scripts/cornerExpand.cs
@@ -21,26 +21,50 @@ public class cornerExpand : MonoBehaviour
         right = transform.GetChild(2).gameObject;
         left = transform.GetChild(3).gameObject;
 
+        Collider[] collider = Physics.OverlapSphere(transform.position, 3f);
+
+        if(collider.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+
         expand();
     }
 
     public void expand()
     {
-        GameObject Front = Instantiate(hall, new Vector3(front.transform.position.x, front.transform.position.y, front.transform.position.z), front.transform.rotation, gameObject.transform);
-        hallExtender hallExtender = Front.GetComponent<hallExtender>();
-        hallExtender.cornerExpand = gameObject.GetComponent<cornerExpand>();
+        RaycastHit frontHit;
+        if (!Physics.Raycast(new Vector3(front.transform.position.x, front.transform.position.y + 2, front.transform.position.z), Vector3.down, out frontHit))
+        {
+            GameObject Front = Instantiate(hall, new Vector3(front.transform.position.x, front.transform.position.y, front.transform.position.z), front.transform.rotation, gameObject.transform);
+            hallExtender hallExtender = Front.GetComponent<hallExtender>();
+            hallExtender.cornerExpand = gameObject.GetComponent<cornerExpand>();
+        }
 
-        GameObject Back = Instantiate(hall, new Vector3(back.transform.position.x, back.transform.position.y, back.transform.position.z), back.transform.rotation, gameObject.transform);
-        hallExtender hallExtender1 = Back.GetComponent<hallExtender>();
-        hallExtender1.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        RaycastHit backHit;
+        if (!Physics.Raycast(new Vector3(back.transform.position.x, back.transform.position.y + 2, back.transform.position.z), Vector3.down, out backHit))
+        {
+            GameObject Back = Instantiate(hall, new Vector3(back.transform.position.x, back.transform.position.y, back.transform.position.z), back.transform.rotation, gameObject.transform);
+            hallExtender hallExtender1 = Back.GetComponent<hallExtender>();
+            hallExtender1.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        }
 
-        GameObject Right = Instantiate(hall, new Vector3(right.transform.position.x, right.transform.position.y, right.transform.position.z), right.transform.rotation, gameObject.transform);
-        hallExtender hallExtender2 = Right.GetComponent<hallExtender>();
-        hallExtender2.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        RaycastHit rightHit;
+        if (!Physics.Raycast(new Vector3(right.transform.position.x, right.transform.position.y + 2, right.transform.position.z), Vector3.down, out rightHit))
+        {
+            GameObject Right = Instantiate(hall, new Vector3(right.transform.position.x, right.transform.position.y, right.transform.position.z), right.transform.rotation, gameObject.transform);
+            hallExtender hallExtender2 = Right.GetComponent<hallExtender>();
+            hallExtender2.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        }
 
-        GameObject Left = Instantiate(hall, new Vector3(left.transform.position.x, left.transform.position.y, left.transform.position.z), left.transform.rotation, gameObject.transform);
-        hallExtender hallExtender3 = Left.GetComponent<hallExtender>();
-        hallExtender3.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        RaycastHit leftHit;
+        if (!Physics.Raycast(new Vector3(left.transform.position.x, left.transform.position.y + 2, left.transform.position.z), Vector3.down, out leftHit))
+        {
+            GameObject Left = Instantiate(hall, new Vector3(left.transform.position.x, left.transform.position.y, left.transform.position.z), left.transform.rotation, gameObject.transform);
+            hallExtender hallExtender3 = Left.GetComponent<hallExtender>();
+            hallExtender3.cornerExpand = this.gameObject.GetComponent<cornerExpand>();
+        }
 
         expandingIterations = 1;
     }
